f63c92fde7ef1c9a21534210fd8d667d7249db47
Luiz Rodrigues <atende@webvirtua.com.br>
2023-05-25 11:43:38 -0300

    fix: fixed problem when try attaching static IP on instance

--- data-source.tf
+++ data-source.tf
@@ -1,6 +1,4 @@
 data "oci_core_vnic_attachments" "get_vnic_attachments" {
-  count = var.has_public_ip ? 1 : 0
-
   compartment_id      = var.compartment_id
   availability_domain = var.availability_domain
   instance_id         = oci_core_instance.create_vm.id
@@ -13,7 +11,7 @@ data "oci_core_vnic_attachments" "get_vnic_attachments" {
 data "oci_core_vnic" "create_vnic" {
   count = length(data.oci_core_vnic_attachments.get_vnic_attachments) >= 1 ? 1 : 0
 
-  vnic_id = data.oci_core_vnic_attachments.get_vnic_attachments[0].vnic_attachments[0]["vnic_id"]
+  vnic_id = data.oci_core_vnic_attachments.get_vnic_attachments.vnic_attachments[0]["vnic_id"]
 
   depends_on = [
     data.oci_core_vnic_attachments.get_vnic_attachments
